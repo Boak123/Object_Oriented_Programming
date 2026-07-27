44f62c39a87a40247db66974731bbfc2b2f9989b
Boak123 <boluwatifeakorede17@gmail.com>
2026-07-27 01:09:43 +0100

    oop Practices

--- student.py
+++ student.py
@@ -1,12 +1,107 @@
-class Student:
+# class Student:
+#     pass
+
+
+# student1 = Student()
+# student2 = Student()
+
+# print(student1)
+# print(student2)
+
+# print(type(student1))
+# print(type(student2))
+
+
+class Car:
     pass
 
 
-student1 = Student()
-student2 = Student()
+my_car = Car()
+print(my_car)
 
-print(student1)
-print(student2)
+class Books:
+    pass
 
-print(type(student1))
-print(type(student2))
+
+title = Books()
+color = Books()
+pages = Books()
+
+print(title)
+print(color)
+print(pages)
+
+
+class Laptop:
+    pass
+
+laptop1 = Laptop()
+laptop2 = Laptop()
+laptop3 = Laptop()
+laptop4 = Laptop()
+
+print(type(laptop1))
+print(type(laptop2))
+
+
+class Student:
+    pass
+
+name = Student()
+print(name)
+
+class Teacher:
+    pass
+
+subject = Teacher
+print(subject)
+
+class Course:
+    pass
+
+python = Course()
+print(python)
+
+
+patient_list = []
+
+class Patient:
+    pass
+
+for i in range(10):
+    patient = Patient()
+    patient_list.append(patient)
+print(patient_list)
+
+class Student:
+    pass
+
+name = Student()
+print(name)
+print(type(name))
+
+class Lecturer:
+    pass
+
+subject = Lecturer()
+print(subject)
+print(type(subject))
+
+
+# it will print the type or blueprint of dog1 work and print and print out dog1 and dog2
+
+
+# the reason i'm not doing the other ones is that i have already apply those concept to the tasks I have done before
+
+class Car:
+    pass
+
+car1 = Car()
+
+garage = []
+
+for i in range(3):
+    garage.append(car1)
+
+print(garage)
+print(type(car1))
