a3693141629dcc497fea7d28b1087ac4f29c275d
Boak123 <boluwatifeakorede17@gmail.com>
2026-07-28 10:29:41 +0100

    debuuging in OOP

---
+++ oop_debugging.py
@@ -0,0 +1,13 @@
+# Find and fix the errors:
+
+class Student:
+    pass
+
+student = Student()
+
+student.name = "Victor"
+
+print(student.name)
+
+# Explain each correction: the only correction I made was add the bracket to identify the Student is the blueprint or class in Line 6
+
