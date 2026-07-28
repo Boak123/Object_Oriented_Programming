be165cb83b5bd27c79d5eaac2029d5f86a6797e9
Boak123 <boluwatifeakorede17@gmail.com>
2026-07-28 10:28:55 +0100

    attributes problem solving

---
+++ class_attributes.py
@@ -0,0 +1,93 @@
+class Car:
+    pass
+
+Car1 = Car()
+Car2 = Car()
+
+brand_1 = Car1.brand = "Toyotal"
+color1 = Car1.color = "Black"
+year1 = Car1.year = "2021"
+
+brand2 = Car2.brand = "Honda"
+color2 = Car2.color = "White"
+year2 = Car2.year = "2020"
+
+print(brand_1)
+print(color1)
+print(year1)
+
+print(brand2)
+print(color2)
+print(year2)
+
+# reason i did not solve the other biginner question is that i think the concept is the same thing as question one
+
+class Student:
+    pass
+
+Student1 = Student()
+Student2 = Student()
+
+Student1.name = "boluwatife victor"
+Student1.course = "Computer Science"
+Student1.age = 19
+
+Student2.name = "victor boluwatife"
+Student2.course = "Information Technogy"
+Student2.age = 20
+
+class Laptop:
+    pass
+
+Laptop1 = Laptop()
+Laptop2 = Laptop()
+
+RAM = Laptop1.ram = 16
+print(RAM)
+
+RAM = Laptop1.ram = 32
+print(RAM)
+
+"""Design a Product class for an online store.
+
+Create three products with:
+name
+price
+stock"""
+
+class Product:
+    pass
+
+Product1 = Product()
+Product2 = Product()
+Product3 = Product()
+
+Product1.name = "Iphone16"
+Product1.price = "$2000"
+Product1.stock = 14
+
+# same apply to the other product but different name price and stock
+
+"""Design an Employee class.
+
+Create two employees.
+Give each:
+name
+department
+salary"""
+
+class Employee:
+    pass
+
+Employee1 = Employee()
+Employee2 = Employee()
+
+Employee1.name = "Victor Ojo"
+Employee1.department = "customer care"
+Employee1.salary = 50000
+
+# same apply to the other Employee but different name, department and salary
+
+
+
+
