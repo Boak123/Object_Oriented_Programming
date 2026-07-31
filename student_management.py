e1147340e5a24ca57107fc8f9e0592b34cace46c
Boak123 <boluwatifeakorede17@gmail.com>
2026-07-31 23:46:45 +0100

    student management projects start

---
+++ student_management.py
@@ -0,0 +1,22 @@
+class Student:
+
+    def study(self):
+        print("I am Studying")
+
+    def take_exam(self):
+        print("I am taking an exam.")
+
+    def register(self):
+        print("I have registered my courses.")
+
+    def sleep(self):
+        print("I want to sleep")
+    def eat(self):
+        print("I want to eat")
+
+student1 = Student()
+student1.study()
+student1.take_exam()
+student1.register()
+student1.sleep()
+student1.eat()
