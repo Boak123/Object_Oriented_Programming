1a1052b2036b605cc3dc9748eda065c5f18c9d22
Boak123 <boluwatifeakorede17@gmail.com>
2026-07-31 23:47:57 +0100

    methods

--- methods/oop_methods.py
+++ methods/oop_methods.py
@@ -1,1 +1,79 @@
-# A method is a function that belongs to a class and defines what an object can do.
+class Student:
+
+    def study(self):
+        print("I am studying Python.")
+
+
+student1 = Student()
+
+student1.study()
+
+class Car:
+
+    def start(self):
+        print("start the call")
+
+    def stop(self):
+        print("stop the card")
+
+car1 = Car()
+car1.start()
+car1.stop()
+
+class Book:
+
+    def read(self):
+        print("I want to read my book")
+
+python_for_beginner = Book()
+Complet_API_for_Beginner = Book()
+python_for_beginner.read()
+Complet_API_for_Beginner.read()
+
+class BankAccount:
+
+    def deposit(self):
+        print("$5000")
+    def withdraw(self):
+        print("$2000")
+    def check_balance(self):
+        print("$3000")
+
+acount1 = BankAccount()
+
+class Employee:
+
+    def work(slf):
+        print()
+
+    def attend_meeting(self):
+        print()
+    def go_home(self):
+        print()
+
+employee1 = Employee
+
+class User:
+
+    def login(self):
+        print("successfully Login")
+    def logout(self):
+        print("successfully Log Out")
+
+user1 = User()
+user2 = User()
+user1.username = "bolu_1"
+user1.email = "boluwatifevictor111@gamil.com"
+user1.login()
+user1.logout()
+
+class Product:
+
+    def display_product(self):
+        pass
+
+    def update_price(self):
+        print("Price updated.")
+
+product1 = Product()
+product1.update_price()
