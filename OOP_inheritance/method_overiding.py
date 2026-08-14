5ae5db02eb9e11c3a6fff8df625273e7f177823b
Boak123 <boluwatifeakorede17@gmail.com>
2026-08-14 23:30:09 +0100

    Overiding with attributes

--- OOP_inheritance/method_overiding.py
+++ OOP_inheritance/method_overiding.py
@@ -1,18 +1,51 @@
+# # Overiding
+
+# class Person:
+#     def introduce(self):
+#         print("I am a Person")
+
+# class Student(Person):
+#     def introduce(self):
+#         print("I am a student")
+
+# class Lecturer(Person):
+#     def introduce(self):
+#         print("I am a Lecturer")
+
+# class Administrator(Person):
+#     def introduce(self):
+#         print("I am an Administrator")
+
+
+# person1 = Person()
+# person1.introduce()
+# student1 = Student()
+# student1.introduce()
+# lecturer1 = Lecturer()
+# lecturer1.introduce()
+# administrator1 = Administrator()
+# administrator1.introduce()
+
+# Overiding with attributes
+
 class Person:
+
+    def __init__(self, name):
+        self.name = name
+
     def introduce(self):
-        print("I am a Person")
+        print(f"I am {self.name}")
 
 class Student(Person):
-    def introduce(self):
-        print("I am a student")
+    def __init__(self, name, student_id):
+        self.name = name
+        self.student_id = student_id
 
-class Lecturer(Person):
     def introduce(self):
-        print("I am a Lecturer")
+        print(f"I am {self.name} and student ID of {self.student_id}")
 
-class Administrator(Person):
-    def introduce(self):
-        print("I am an Administrator")
+persongg = Person("BOLU")
+persongg.introduce()
+studentrr = Student("Bolu", 1234)
+studentrr.introduce()
 
-student1 = Student()
-student1.introduce()
