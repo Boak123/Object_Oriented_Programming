075108fcc5dda55d5df97c4c555280a8bea3c5aa
Boak123 <boluwatifeakorede17@gmail.com>
2026-08-16 03:19:20 +0100

    plymorphosm and attributes

--- OOP_inheritance/employee.py
+++ OOP_inheritance/employee.py
@@ -1,7 +1,34 @@
 class Employee:
 
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
     def work(self):
-        pass
+        print(f"I am Working with {self.name}")
+
+    def calculate_bonus(self):
+        bonus = self.salary * 5 / 100
+        return bonus
 
 class Manager(Employee):
-    pass
+
+    def calculate_bonus(self):
+        bonus = self.salary * 15 / 100
+        return bonus
+
+class Developer(Employee):
+    def calculate_bonus(self):
+        bonus = self.salary * 10 / 100
+        return bonus
+
+employee1 = Employee("Mr Jones", 5000)
+manager = Manager("Mr Victor", 4000)
+developer1 = Developer("Mr Bolu", 3000)
+
+employees = [employee1, manager, developer1]
+
+for employee in employees:
+    employee.work()
+    print(employee.calculate_bonus())
+    print("----------------")
