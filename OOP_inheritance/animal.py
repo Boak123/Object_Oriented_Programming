9da5e3b2b743562e44fe2be70a46ab63fcd9e5b3
Boak123 <boluwatifeakorede17@gmail.com>
2026-08-17 22:34:44 +0100

    updated

--- OOP_inheritance/animal.py
+++ OOP_inheritance/animal.py
@@ -1,6 +1,7 @@
 class Animal:
-    def __init__(self, name):
+    def __init__(self, name, makeSound):
         self.name = name
+        self.makeSound = makeSound
 
     def eat(self):
         print("eating")
@@ -8,13 +9,39 @@ class Animal:
     def sleep(self):
         print("Sleeping")
 
+    def introduce(self):
+        print(f"I am an {self.name}")
+
 class Dog(Animal):
 
-    def bark(self):
-        print("Barking")
+    def introduce(self):
+        print(f"I am a {self.name}")
 
-dog1 = Dog("jack")
-dog1.sleep()
-dog1.eat()
-dog1.bark()
+    def make_sound(self):
+            print(f"{self.makeSound}")
 
+class Cat(Animal):
+
+    def introduce(self):
+        print(f"I am a {self.name}")
+
+    def make_sound(self):
+        print(f"{self.makeSound}")
+
+class Bird(Animal):
+
+    def introduce(self):
+        print(f"I am a {self.name}")
+    def make_sound(self):
+        print(f"{self.makeSound}")
+
+
+dog1 = Dog("jack", "Bark")
+cat1 = Cat("jerry", "Meow")
+bird1 = Bird("Pigeon", "Chirp")
+
+
+animal = [dog1, cat1, bird1]
+
+for ani in animal:
+    ani.make_sound()
