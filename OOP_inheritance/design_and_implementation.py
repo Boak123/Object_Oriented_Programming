7904b6e2aade1e2e04acb304084ffb68344ccbea
Boak123 <boluwatifeakorede17@gmail.com>
2026-08-17 22:33:37 +0100

    design and implementation

---
+++ OOP_inheritance/design_and_implementation.py
@@ -0,0 +1,40 @@
+"""You're designing the system yourself.
+
+Scenario
+
+A university wants a simple Payment System.
+
+There are three types of people who can receive payments:
+
+Person
+├── Student
+├── Lecturer
+└── Administrator
+
+Every person has:
+
+name
+account_number
+
+Every class must have:
+
+receive_payment()
+
+But the behavior must be different:
+
+Student
+
+Receives payment and prints:
+
+Victor received ₦50000 as student allowance.
+Lecturer
+
+Receives payment and prints:
+
+Bolu received ₦300000 as lecturer salary.
+Administrator
+
+Receives payment and prints:
+
+Tola received ₦200000 as administrator salary."""
+
